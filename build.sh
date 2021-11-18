624f761c6d6b3cce1b3c5c80c55f6e30cfe4d6a2
Thijs Eilander <eilander@myguard.nl>
2021-11-18 23:15:48 +0100

    cleanup

--- build.sh
+++ build.sh
@@ -5,34 +5,15 @@ WORKDIR=$(pwd)
 mkdir -p src
 cd src
 
-#if [ ! -d "master" ]; then
-#    echo "cloning.."
-#    git clone -b latest-stable --depth=1 -c advice.detachedHead=false --recursive https://github.com/apache/incubator-pagespeed-mod.git
-#else
-#    echo "pulling.."
-#    cd incubator-pagespeed-mod
-#    git pull --recurse-submodules
-#fi
-
 if [ ! -d "master" ]; then
     echo "cloning.."
     git clone --depth=10 -c advice.detachedHead=false --recursive https://github.com/apache/incubator-pagespeed-mod.git master
-    sed -i '1s/^/#include <cstdarg>\n/' pagespeed/kernel/base/string_util.cc
+    sed -i s/"#include <string>"/"#include <string>\n#include <cstdarg>"/ pagespeed/kernel/base/string.h
 else
     echo "pulling.."
     cd master
     git pull --recurse-submodules
 fi
-
-cd ${WORKDIR}/src/master
-
-
-
-#echo "copying third_party for now"
-#cp -rp incubator-pagespeed-mod/third_party master
-
-# add #include <cstdarg>
-sed -i s/"#include <string>"/"#include <string>\n#include <cstdarg>"/ pagespeed/kernel/base/string.h
 
 DIST=focal
 
@@ -41,7 +22,5 @@ cd ${WORKDIR}
 cp docker/Dockerfile-template docker/Dockerfile
 sed -i s/DIST/${DIST}/ docker/Dockerfile
 
-docker build --no-cache -t eilandert/psol:${DIST} docker
-#docker push eilandert/psol:${DIST}
-docker run --volume ${WORKDIR}/src:/usr/src eilandert/psol:${DIST}
-
+docker build --no-cache -t psol:${DIST} docker
+docker run --volume ${WORKDIR}/src:/usr/src psol:${DIST}
